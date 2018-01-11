157fa326da43d1b0c8311bac49bdca67357e3d2c
Milosz Radziszewski <milosz.radziszewski@flex.com>
2018-01-11 01:11:45 -0800

    [Studio] Add screen "ReportsList"

---
+++ client/controllers/app/reportslist.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_reportslist', app_reportslist);
+function app_reportslist($scope, app) {
+    'use strict';
+    app.init($scope);
+}
---
+++ client/mocks/reportslist_2.js
@@ -0,0 +1,3 @@
+'use strict';
+window.mocks = window.mocks || {};
+window.mocks.reportslist_2 = window.mocks.reportslist_2 || {};
