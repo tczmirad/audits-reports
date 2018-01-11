ccefa7966ffa894349393a67c4d42df18f84cfde
Milosz Radziszewski <milosz.radziszewski@flex.com>
2018-01-10 21:12:32 -0800

    [Studio] Add screen "ReportsList"

---
+++ client/mocks/reportslist.js
@@ -0,0 +1,3 @@
+'use strict';
+window.mocks = window.mocks || {};
+window.mocks.reportslist = window.mocks.reportslist || {};
--- client/states.js
+++ client/states.js
@@ -33,5 +33,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.reportslist', {
+        views: {
+            app: {
+                controller: 'app_reportslist',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.reportslist');
+                }
+            }
+        }
     });
 });
