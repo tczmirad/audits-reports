288c65b5e146c2e1151858af3e96e434538a84b1
Milosz Radziszewski <milosz.radziszewski@flex.com>
2018-01-12 05:23:57 -0800

    [Studio] Update mock actions for "ReportDetails", Modified screen "ReportDetails"

--- client/mocks/ReportDetails.js
+++ client/mocks/ReportDetails.js
@@ -1,3 +1,6 @@
 'use strict';
 window.mocks = window.mocks || {};
 window.mocks.ReportDetails = window.mocks.ReportDetails || {};
+window.mocks.ReportDetails.PageBack = function ($scope, params, app) {
+    app.go('app.reportslist');
+};
