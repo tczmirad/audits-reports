b08da91837c208186522364f99284755b15c87e8
Milosz Radziszewski <milosz.radziszewski@flex.com>
2018-01-12 06:24:55 -0800

    [Studio] Modified screen "ReportDetails"

--- client/controllers/app/reportdetails.js
+++ client/controllers/app/reportdetails.js
@@ -2,4 +2,8 @@ angular.module('app').controller('app_reportdetails', app_reportdetails);
 function app_reportdetails($scope, app) {
     'use strict';
     app.init($scope);
+    
+    $scope.goBack = function(){
+        app.call("details.pageBack")
+    }
 }
