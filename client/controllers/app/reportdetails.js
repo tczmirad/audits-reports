af361302b1649cb079a5eaee5c4c016cd681497d
Milosz Radziszewski <milosz.radziszewski@flex.com>
2018-01-12 05:50:20 -0800

    [Studio] Modified screen "ReportDetails"

--- client/controllers/app/reportdetails.js
+++ client/controllers/app/reportdetails.js
@@ -1,9 +1,7 @@
 angular.module('app').controller('app_reportdetails', app_reportdetails);
-function app_reportdetails($scope, app, page) {
+function app_reportdetails($scope, app) {
     'use strict';
     app.init($scope);
-    $scope.test = function(){
-        console.log(page);
-    }
+   
   
 }
