552df69af0d4517fad3561cda25fd1821533be12
Milosz Radziszewski <milosz.radziszewski@flex.com>
2018-01-12 05:26:06 -0800

    [Studio] Update mock actions for "Landing"

--- client/mocks/Landing.js
+++ client/mocks/Landing.js
@@ -1,3 +1,6 @@
 'use strict';
 window.mocks = window.mocks || {};
 window.mocks.Landing = window.mocks.Landing || {};
+window.mocks.Landing.Reports_Click = function ($scope, params, app) {
+    app.go('app.reportdetails');
+};
